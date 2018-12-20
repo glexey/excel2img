3950bfbb142fc7b956f3edf636e182f7347e9710
aagaidiu <alexey.a.gaidiukov@intel.com>
2018-12-20 09:58:25 -0800

    Basic testing

--- tests/test_excel2img.py
+++ tests/test_excel2img.py
@@ -6,4 +6,7 @@ os.chdir(mypath)
 
 def test_all():
     import excel2img
+    fnout = "test.png"
+    if os.path.exists(fnout): os.unlink(fnout)
     excel2img.export_img("test.xlsx", "test.png", "Sheet1", None)
+    assert os.path.exists(fnout)
